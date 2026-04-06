f42c9b50305682070820c7cc87a1f3303397a4d2
dikie001 <omondidickens255@gmail.com>
2026-04-06 16:44:43 +0300

    feat: remove login and registration pages and their routes

--- client/src/routes/register.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoute, lazyRouteComponent } from "@tanstack/react-router";
-import { rootRoute } from "./root";
-
-export const registerRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/register",
-  component: lazyRouteComponent(() => import("@/features/auth/pages/register")),
-});
