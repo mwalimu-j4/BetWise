926a602aaffe53e2b401dea62bbbf1ad0be122d1
dikie001 <omondidickens255@gmail.com>
2026-04-17 04:47:45 +0300

    fix(profile): streamline ProfileAliasRoute component

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -2,9 +2,7 @@ import { Navigate, createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./root";
 
 function ProfileAliasRoute() {
-  return (
-    <Navigate to="/user/profile" />
-  );
+  return <Navigate to="/user/profile" />;
 }
 
 export const profileRoute = createRoute({
